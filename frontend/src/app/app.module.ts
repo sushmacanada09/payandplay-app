d8ef7db6cb9101b2c3b5bfa3569c9e56fc1966d8
sushmacanada09 <161357098+sushmacanada09@users.noreply.github.com>
2024-06-29 01:16:29 -0400

    login page initial design

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { AddCartComponent } from './add-cart/add-cart.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    AddCartComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
