b6ffbe64b8ec133f9d064021be578ad6a61ae2f0
sushmacanada09 <161357098+sushmacanada09@users.noreply.github.com>
2024-07-05 23:12:22 -0400

    home navigation commit

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute,Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit{
+currentUrl: string;
+isBackGrd:boolean;
+  constructor(private route: ActivatedRoute, private router: Router) {
+    
+  }
+  ngOnInit() {
+    this.currentUrl =  this.router.url;
+    console.log(this.currentUrl);
+  }
+
+  isLoginPage(): boolean {
+    return this.router.url == '/login';
+  }
 }
